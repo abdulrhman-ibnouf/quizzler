d8c81b5d821037aeb5e3854ea3bcaefc59a8215a
abdulrhman-ibnouf <ibnouf88@gmail.com>
2020-03-22 22:46:41 +0200

    Thank you Angela from London App Brewery.

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:quizzler/quiz_brain.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 
 QuizBrain quizBrain = QuizBrain();
 
-void main() =>
-    runApp(MaterialApp(
+void main() => runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Quizzler',
-      home: Scaffold(backgroundColor: Colors.black, body: QuizPage(),),));
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: QuizPage(),
+      ),
+    ));
 
 class QuizPage extends StatefulWidget {
   @override
@@ -15,83 +19,113 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
-
   List<Icon> scoreKeeper = [];
-  int questionNumber = 0;
+
+  void checkAnswer(bool pickedAnswer) {
+    bool correctAnswer = quizBrain.getQuestionAnswer();
+    setState(() {
+      if (quizBrain.checkLastQuestion() == true) {
+        Alert(context: context, title: "Quiz Finished", desc: "The Questions bank is finished, wait untill we fill it again for you.").show();
+        scoreKeeper = [];
+        quizBrain.getFirstQuestion();
+      } else {
+        if (pickedAnswer == correctAnswer) {
+          scoreKeeper.add(
+            Icon(
+              Icons.check,
+              color: Colors.green,
+            ),
+          );
+        } else {
+          scoreKeeper.add(
+            Icon(
+              Icons.close,
+              color: Colors.red,
+            ),
+          );
+        }
+        quizBrain.getNextQuestion();
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Padding(padding: const EdgeInsets.all(16.0),
-      child: Column(mainAxisAlignment: MainAxisAlignment.center,
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           //for the question
-          Expanded(flex: 5,
-            child: Center(child: Text(
-              quizBrain.questionBank[questionNumber].questionText, textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.white,
-                fontSize: 25,
-                fontWeight: FontWeight.bold,),),),),
-          //for the choices
-          Expanded(flex: 2,
+          Expanded(
+            flex: 5,
+            child: Center(
+              child: Text(
+                quizBrain.getQuestionText(),
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ), //for the choices
+          Expanded(
+            flex: 2,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: <Widget>[
-                Row(mainAxisAlignment: MainAxisAlignment.spaceAround,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: <Widget>[
-                    Expanded(flex: 1,
-                      child: ClipRRect(borderRadius: BorderRadius.only(topLeft: Radius.circular(50),),
+                    Expanded(
+                      flex: 1,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(50),
+                        ),
                         child: FlatButton(
                           padding: EdgeInsets.fromLTRB(16, 16, 0, 16),
                           color: Colors.green,
                           textColor: Colors.white,
-                          child: Text('True', style: TextStyle(color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,),),
+                          child: Text(
+                            'True',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            ),
+                          ),
                           onPressed: () {
-                            bool correctAnswer = quizBrain.questionBank[questionNumber].questionAnswer;
-                            if (correctAnswer == true) {
-                              setState(() {
-                                scoreKeeper.add(
-                                  Icon(Icons.check, color: Colors.green,),);
-                              });
-                            } else {
-                              setState(() {
-                                scoreKeeper.add(
-                                  Icon(Icons.close, color: Colors.red,),);
-                              });
-                            }
-                            setState(() {
-                              questionNumber++;
-                            });
-                          },),),),
-                    Expanded(flex: 1,
-                      child: ClipRRect(borderRadius: BorderRadius.only(topRight: Radius.circular(50),),
+                            checkAnswer(true);
+                          },
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(50),
+                        ),
                         child: FlatButton(
                           padding: EdgeInsets.fromLTRB(0, 16, 16, 16),
                           color: Colors.red,
                           textColor: Colors.white,
-                          child: Text('False', style: TextStyle(color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,),),
+                          child: Text(
+                            'False',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            ),
+                          ),
                           onPressed: () {
-                            bool correctAnswer = quizBrain.questionBank[questionNumber].questionAnswer;
-                            if (correctAnswer == false) {
-                              setState(() {
-                                scoreKeeper.add(
-                                  Icon(Icons.check, color: Colors.green,),);
-                              });
-                            } else {
-                              setState(() {
-                                scoreKeeper.add(
-                                  Icon(Icons.close, color: Colors.red,),);
-                              });
-                            }
-                            setState(() {
-                              questionNumber++;
-                            });
+                            checkAnswer(false);
                           },
                         ),
                       ),
@@ -99,27 +133,38 @@ class _QuizPageState extends State<QuizPage> {
                   ],
                 ),
                 ClipRRect(
-                  borderRadius: BorderRadius.only(bottomLeft: Radius.circular(50), bottomRight: Radius.circular(50),),
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(50),
+                    bottomRight: Radius.circular(50),
+                  ),
                   child: FlatButton(
                     padding: EdgeInsets.all(20),
                     color: Colors.deepPurple,
                     textColor: Colors.white,
-                    child: Text('Reset',), onPressed: (){
+                    child: Text(
+                      'Reset',
+                    ),
+                    onPressed: () {
                       setState(() {
                         scoreKeeper.clear();
-                        questionNumber = 0;
+                        quizBrain.getFirstQuestion();
                       });
-                  },
+                    },
                   ),
                 ),
               ],
             ),
-          ),
-          //for the score keeper
-          Container(height: 25,
-            child: Row(mainAxisAlignment: MainAxisAlignment.start,
+          ), //for the score keeper
+          Container(
+            height: 25,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.center,
-              children: scoreKeeper,),)
-        ],),);
+              children: scoreKeeper,
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
